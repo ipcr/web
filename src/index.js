bf10d50bc130cd001b23d32fa038d6b4bc25e329
Elisey Zanko <elisey.zanko@gmail.com>
2020-07-27 21:40:37 +0500

    Show image description

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ReactDOM from 'react-dom';
 import React from 'react';
+import {
+    HashRouter as Router,
+    Switch,
+    Route,
+    Link,
+} from "react-router-dom";
 import IPFS from 'ipfs';
+
+const REGISTRY_CID = '/ipfs/QmdtQjb1hRxH5KUdYVxktTG9q3LseiQJEjhy9jeWNVqA79'
 
 class ErrorBoundary extends React.PureComponent {
     constructor(props) {
@@ -28,6 +36,64 @@ class ErrorBoundary extends React.PureComponent {
     }
 }
 
+class Loading extends React.PureComponent {
+    render() {
+        return (
+            <div className="text-center">
+              <div className="spinner-border" role="status">
+                <span className="sr-only">Loading...</span>
+              </div>
+            </div>
+        );
+    }
+}
+
+class ImageDetails extends React.PureComponent {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            image: null,
+            error: null,
+        };
+    }
+
+    async componentDidMount() {
+        const { params } = this.props.match;
+        try {
+            const descriptionCID = REGISTRY_CID + '/' + params.image  + '/' + 'README-short.txt';
+            const chunks = [];
+
+            for await (const chunk of this.props.ipfs.cat(descriptionCID)) {
+                chunks.push(chunk);
+            }
+            const description = Buffer.concat(chunks).toString();
+
+            this.setState({image: {
+                name: params.image,
+                description: description,
+            }});
+        } catch (error) {
+            this.setState({error: error});
+        }
+    }
+
+    render() {
+        if (this.state.error) {
+            throw this.state.error;
+        }
+        if (this.state.image) {
+            return (
+                <div>
+                    <h1>{this.state.image.name}</h1>
+                    <h2>{this.state.image.description}</h2>
+                </div>
+            );
+        }
+        return <Loading />;
+    }
+}
+
 class ImageList extends React.PureComponent {
     constructor(props) {
         super(props);
@@ -40,10 +106,7 @@ class ImageList extends React.PureComponent {
 
     async componentDidMount() {
         try {
-            const ipfs = await IPFS.create();
-            
-            for await (const img of ipfs.ls('/ipfs/QmdtQjb1hRxH5KUdYVxktTG9q3LseiQJEjhy9jeWNVqA79')) {
-                console.log(img);
+            for await (const img of this.props.ipfs.ls(REGISTRY_CID)) {
                 this.setState(state => {
                     return {
                         images: [...state.images, img],
@@ -64,32 +127,61 @@ class ImageList extends React.PureComponent {
             return (
                 <div>
                     {this.state.images.map(img => (
-                        <a href="#" className="btn btn-light btn-lg btn-block" key={img.path}>{img.name}</a>
+                        <Link className="btn btn-light btn-lg btn-block" key={img.path} to={img.name}>{img.name}</Link>
                     ))}
                 </div>
             );
         }
-        return (
-            <div className="text-center">
-              <div className="spinner-border" role="status">
-                <span className="sr-only">Loading...</span>
-              </div>
-            </div>
-        );
+        return <Loading />;
     }
 }
 
 class App extends React.PureComponent {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            ipfs: null,
+            error: null,
+        };
+    }
+
+    async componentDidMount() {
+        try {
+            const ipfs = await IPFS.create();
+            this.setState({ipfs: ipfs});
+        } catch (error) {
+            this.setState({error: error});
+        }
+    }
+
+    async componentWillUnmount() {
+        if (this.state.ipfs) {
+            await this.state.ipfs.stop()
+        }
+    }
+
     render() {
+        if (this.state.error) {
+            throw this.state.error;
+        }
+        if (!this.state.ipfs) {
+            return <Loading />;
+        }
         return (
-            <ErrorBoundary>
-                <ImageList/>
-            </ErrorBoundary>
+            <Switch>
+                <Route exact path="/" render={(props) => <ImageList {...props} ipfs={this.state.ipfs} />} />
+                <Route path="/:image" render={(props) => <ImageDetails {...props} ipfs={this.state.ipfs} />} />
+            </Switch>
         );
     }
 }
 
 ReactDOM.render(
-    <App/>,
+    <Router>
+        <ErrorBoundary>
+            <App/>
+        </ErrorBoundary>
+    </Router>,
     document.getElementById('app')
 );
